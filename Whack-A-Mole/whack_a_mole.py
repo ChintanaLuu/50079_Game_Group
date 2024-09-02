6143fa4b18c1e2424de247e2ecd208086fbf0c56
ChintanaLuu <luucy003@mymail.unisa.edu.au>
2024-09-02 16:54:22 +0930

    Add Mole On Screen
    Draw mole on screen.

--- Whack-A-Mole/whack_a_mole.py
+++ Whack-A-Mole/whack_a_mole.py
@@ -19,6 +19,17 @@ while True:
             pygame.quit()
             exit()
 
+    pygame.Surface.fill(screen, (0,255,0))
+    # Draw the mole.
+    mole_sprite = pygame.image.load('Game_Art\R (9).png').convert_alpha()
+    screen.blit(mole_sprite, (0,0))
+    
+    # Make mole clickable.
+    mole_rect = mole_sprite.get_rect()
+
     # Update display screen.
     pygame.display.update()
     clock.tick(60)
+
+
+
