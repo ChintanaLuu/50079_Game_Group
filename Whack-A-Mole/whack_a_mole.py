485eb3979c5bfc098f914248ed8a11c0d4a492b6
ChintanaLuu <luucy003@mymail.unisa.edu.au>
2024-09-02 16:59:59 +0930

    Feat Clickable Mole

--- Whack-A-Mole/whack_a_mole.py
+++ Whack-A-Mole/whack_a_mole.py
@@ -19,13 +19,20 @@ while True:
             pygame.quit()
             exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mousepos = pygame.mouse.get_pos()
+            if mole_rect.collidepoint(mousepos):
+                print("MOLE CAUGHT!")
+
     pygame.Surface.fill(screen, (0,255,0))
     # Draw the mole.
     mole_sprite = pygame.image.load('Game_Art\R (9).png').convert_alpha()
     screen.blit(mole_sprite, (0,0))
-    
+
     # Make mole clickable.
     mole_rect = mole_sprite.get_rect()
+
+
 
     # Update display screen.
     pygame.display.update()
