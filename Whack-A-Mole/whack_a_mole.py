59a2728bd309659aea6f832171daac656c7df025
ChintanaLuu <luucy003@mymail.unisa.edu.au>
2024-09-02 16:38:25 +0930

    Feat Game Application Window
    Also add placeholder art for game.

--- Whack-A-Mole/whack_a_mole.py
+++ Whack-A-Mole/whack_a_mole.py
@@ -1,1 +1,24 @@
+# Import pygame library
 import pygame
+from sys import exit
+pygame.init()
+
+clock = pygame.time.Clock()
+
+# Define window size.
+windowWidth = 1920
+windowHeight = 1080
+
+# Create app window.
+screen = pygame.display.set_mode((windowWidth, windowHeight))
+
+while True:
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # Update display screen.
+    pygame.display.update()
+    clock.tick(60)
