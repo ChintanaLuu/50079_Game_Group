dcb2b41439ad941ff0344cdc5d5468c5adb38cee
ChintanaLuu <luucy003@mymail.unisa.edu.au>
2024-09-02 18:30:39 +0930

    Add Holes Surface

--- Whack-A-Mole/whack_a_mole.py
+++ Whack-A-Mole/whack_a_mole.py
@@ -45,23 +45,35 @@ while True:
 
     pygame.Surface.fill(screen, (0,255,0))
 
-    # Grid setup.
-    # hole_1 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
-    # # hole_2 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
-    # # hole_3 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
-    # # # hole_4 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
-    # # # hole_5 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
-    # # # hole_6 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    # Grid setup. Should refactor later.
 
+    # Row 1:
 
-    # # # Draw grid.
-    # # for x in range(3):
-    # #     for y in range(3):
+    hole_1 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    hole_1 = pygame.transform.smoothscale_by(hole_1, (0.1,0.1))
+    screen.blit(hole_1, (100,200))
 
-    #hole_1 = pygame.transform.smoothscale_by(hole_1, (0.15,0.15))
-    #screen.blit(hole_1, (500,500)) # WHY IS THERE A LIBPNG WARNING??
-    # screen.blit(hole_2, (500,500))
-    # screen.blit(hole_3, (600,500))
+    hole_2 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    hole_2 = pygame.transform.smoothscale_by(hole_2, (0.1,0.1))
+    screen.blit(hole_2, (450,200))
+
+    hole_3 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    hole_3 = pygame.transform.smoothscale_by(hole_3, (0.1,0.1))
+    screen.blit(hole_3, (800,200))
+
+    # Row 2:
+
+    hole_4 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    hole_4 = pygame.transform.smoothscale_by(hole_4, (0.1,0.1))
+    screen.blit(hole_4, (100,500))
+
+    hole_5 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    hole_5 = pygame.transform.smoothscale_by(hole_5, (0.1,0.1))
+    screen.blit(hole_5, (450,500))
+
+    hole_6 = pygame.image.load('Game_Art\Circle.png').convert_alpha()
+    hole_6 = pygame.transform.smoothscale_by(hole_6, (0.1,0.1))
+    screen.blit(hole_6, (800,500))
 
     # Draw the mole.
     mole_sprite = pygame.image.load('Game_Art\R (9).png').convert_alpha()
@@ -69,9 +81,6 @@ while True:
    # Make mole clickable. Get rect before resizing.
     mole_rect = mole_sprite.get_rect()
     mole_sprite = pygame.transform.smoothscale_by(mole_sprite, (0.1,0.1))
-
- 
-
     screen.blit(mole_sprite, (500,500))
     
 
