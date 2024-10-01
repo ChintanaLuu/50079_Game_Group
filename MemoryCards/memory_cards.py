027a1655ff7e7cf7e9e1032b87fecce515e42114
Son Tung <binlabum123@gmail.com>
2024-10-02 03:34:18 +0930

    integrate the leaderboard modules to the whackamole game

--- MemoryCards/memory_cards.py
+++ MemoryCards/memory_cards.py
@@ -18,14 +18,14 @@ def main_game(player_name):
     def initialize_game():
     # Load images and assign identifiers
         images = {
-            "apple": pygame.image.load("./MemoryCards/images/apple.png"),
-            "banana": pygame.image.load("./MemoryCards/images/banana.png"),
-            "blueberry": pygame.image.load("./MemoryCards/images/blueberry.png"),
-            "eggplant": pygame.image.load("./MemoryCards/images/eggplant.png"),
-            "grape": pygame.image.load("./MemoryCards/images/grape.png"),
-            "lime": pygame.image.load("./MemoryCards/images/lemon.png"),
-            "orange": pygame.image.load("./MemoryCards/images/orange.png"),
-            "pumpkin": pygame.image.load("./MemoryCards/images/pumpkin.png"),
+            "bauble": pygame.image.load("./MemoryCards/images/bauble.png"),
+            "gingerbread": pygame.image.load("./MemoryCards/images/gingerbread.png"),
+            "holly": pygame.image.load("./MemoryCards/images/holly.png"),
+            "milk_and_cookies": pygame.image.load("./MemoryCards/images/milk_and_cookies.png"),
+            "present": pygame.image.load("./MemoryCards/images/present.png"),
+            "santas_hat": pygame.image.load("./MemoryCards/images/santas_hat.png"),
+            "wreath": pygame.image.load("./MemoryCards/images/wreath.png"),
+            "xmasTree": pygame.image.load("./MemoryCards/images/xmasTree.png"),
         }
 
         # Define grid properties
@@ -50,7 +50,8 @@ def main_game(player_name):
             x = col * (card_width + spacing) + spacing
             y = row * (card_height + spacing) + spacing + top_padding
             identifier, image = pairs[i]
-            card = Card(x, y, card_width, card_height, image, identifier)
+            backImage = pygame.image.load("MemoryCards/images/xmas_card_back.png")
+            card = Card(x, y, card_width, card_height, image, identifier, backImage)
             cards.append(card)
         return cards
 
@@ -129,3 +130,5 @@ def main_game(player_name):
         clock.tick(60)  # limits FPS to 60
 
     pygame.quit()
+
+
